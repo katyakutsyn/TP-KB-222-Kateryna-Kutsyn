eb9e83f7d8334dd3a382ae382e9f0288d132bc06
katyakutsyn <kutsyn2005@gmail.com>
2023-10-09 23:26:20 +0300

    change solutions for task1,task2

--- topic_04/task1.py
+++ topic_04/task1.py
@@ -11,20 +11,11 @@ def multiply(a, b):
 
 def divide(a, b):
     try:
-        if b == 0:
-            raise ZeroDivisionError("Division by zero is not allowed")
         return a / b
-    except ZeroDivisionError as e:
-        print("Error:", e)
+    except ZeroDivisionError :
+        print("ZeroDivisionError occurs")
         return None
 
-def getInt(prompt):
-    while True:
-        try:
-            num = int(input(prompt))
-            return num
-        except ValueError:
-            print("Invalid input. Please enter an integer.")
 
 while True:
     print("Choose operation:")
@@ -33,40 +24,35 @@ while True:
     print("3. Multiply = '*' ")
     print("4. Divide = '/' ")
     print("5. Exit = 'Q'")
-    print("6. Get Integer = 'get_int'")
 
     operation = input('Enter operation sign: ')
 
     if operation == "Q":
-        print("GG goodbye")
+        print("Goodbye")
         break
 
-    elif operation == "get_int":
-        num = getInt("Enter an integer: ")
-        print("You entered:", num)
-    else:
-        a = float(input("Enter first number: "))
-        b = float(input("Enter second number: "))
+    try:
+        a = float(input("Enter the first number: "))
+        b = float(input("Enter the second number: "))
 
         result = None
 
-        try:
-            if operation == '+':
-                result = add(a, b)
-            elif operation == '-':
-                result = subtract(a, b)
-            elif operation == '*':
-                result = multiply(a, b)
-            elif operation == '/':
-                result = divide(a, b)
-            else:
-                print("Invalid operation")
-                continue
+        if operation == '+':
+            result = add(a, b)
+        elif operation == '-':
+            result = subtract(a, b)
+        elif operation == '*':
+            result = multiply(a, b)
+        elif operation == '/':
+            result = divide(a, b)
+        else:
+            print("Invalid operation")
+            continue
 
-            if result is not None:
-                print("Result:", result)
+        if result is not None:
+            print("Result:", result)
 
-        except Exception as e:
-            print("Error:", e)
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
 
 
--- topic_04/task2.py
+++ topic_04/task2.py
@@ -1,58 +1,55 @@
 
-import sys
-
 def add(a, b):
     return a + b
 
-def subtract(a, b):
+def sub(a, b):
     return a - b
 
 def multiply(a, b):
     return a * b
 
 def divide(a, b):
-    try:
-        if b == 0:
-            raise ZeroDivisionError("Division by zero is not allowed")
-        return a / b
-    except ZeroDivisionError as e:
-        print("Error:", e)
-        return None
+    if b == 0:
+        raise ValueError("ZeroDivisionError occurs")
+    return a / b
 
 while True:
     print("Choose operation:")
-    print("1. Addition = '+' ")
-    print("2. Subtraction = '-' ")
-    print("3. Multiplication = '*' ")
-    print("4. Division = '/' ")
+    print("1. Add = '+' ")
+    print("2. Subtract = '-' ")
+    print("3. Multiply = '*' ")
+    print("4. Divide = '/' ")
     print("5. Exit = 'Q'")
-    
+
     operation = input('Enter operation sign: ')
 
-    if operation == "Q":
-        print("gg goddbye")
+    if operation == 'Q':
+        print("Goodbye")
         break
-    else:
+
+    if operation not in ('+', '-', '*', '/'):
+        print("Goodbye 2.0")
+        continue
+
+    try:
         a = float(input("Enter the first number: "))
         b = float(input("Enter the second number: "))
 
         result = None
 
-        try:
-            if operation == '+':
-                result = add(a, b)
-            elif operation == '-':
-                result = subtract(a, b)
-            elif operation == '*':
-                result = multiply(a, b)
-            elif operation == '/':
+        if operation == '+':
+            result = add(a, b)
+        elif operation == '-':
+            result = sub(a, b)
+        elif operation == '*':
+            result = multiply(a, b)
+        elif operation == '/':
+            try:
                 result = divide(a, b)
-            else:
-                print("Invalid operation")
+            except ValueError as e:
+                print(f"Error: {e}")
                 continue
 
-            if result is not None:
-                print("Result:", result)
-
-        except Exception as e:
-            print("Error:", e)
+        print("Result:", result)
+    except ValueError:
+        print("Invalid input. Please enter valid numbers.")
