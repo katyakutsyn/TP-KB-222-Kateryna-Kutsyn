786f84ff6d098f2e052bccfa9d819c3e5053a2cd
katyakutsyn <kutsyn2005@gmail.com>
2023-10-09 23:46:09 +0300

    solutin fun with sort for task1 in lab

--- lab_01/lab_01.py
+++ lab_01/lab_01.py
@@ -6,6 +6,7 @@ student_list = [
     {"name": "Puffendui", "phone": "0661234567", "email": "Puffendui@example.com", "group": "A"}
 ]
 
+
 def print_all_students():
     for student in student_list:
         print(f"Name: {student['name']}, Phone: {student['phone']}, Email: {student['email']}, Group: {student['group']}")
@@ -44,9 +45,33 @@ def delete_student():
         print(f"Student '{name}' has been deleted")
     return
 
+def edit_student():
+    name_to_edit = input("Please enter the name of the student to edit: ")
+
+
+    for student in student_list:
+        if student["name"] == name_to_edit:
+            print(f"Editing student: {student['name']}")
+
+            new_name = input("Enter new name: ")
+            new_phone = input("Enter new phone: ")
+            new_email = input("Enter new email: ")
+            new_group = input("Enter new group: ")
+            
+            
+            student["name"] = new_name
+            student["phone"] = new_phone
+            student["email"] = new_email
+            student["group"] = new_group
+            print(f"Student '{name_to_edit}' has been updated")
+            return
+
+
+    print(f"Student '{name_to_edit}' not found")
+
 def main():
     while True:
-        choice = input("Please specify the action [C create, D delete, P print, Q exit]: ")
+        choice = input("Please specify the action [C create, D delete, E edit, P print, Q exit]: ")
         if choice.upper() == "C":
             print("Creating a new student:")
             add_new_student()
@@ -55,14 +80,19 @@ def main():
             print("Deleting a student:")
             delete_student()
             print_all_students()
+        elif choice.upper() == "E":
+            print("Editing a student:")
+            edit_student()
+            print_all_students()
         elif choice.upper() == "P":
             print("Printing the student list:")
             print_all_students()
         elif choice.upper() == "Q":
-            print("Goodbye GG")
+            print("goodbye")
             break
         else:
             print("Invalid choice")
 
 if __name__ == "__main__":
     main()
+
