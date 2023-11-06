fb75227f6ea927c14b2acc4ae970b8c5bf7e962e
katyakutsyn <kutsyn2005@gmail.com>
2023-11-06 22:03:49 +0200

    lab_2 task and csv file new

--- lab_02/lab_02.py
+++ lab_02/lab_02.py
@@ -1,101 +1,88 @@
 import csv
-import sys
 
-student_list = []
-def load_students_from_csv(filename):
-    global student_list
-    try:
-        with open(filename, newline='') as file:
-            reader = csv.DictReader(file)
-            student_list = [row for row in reader]
-        print("Students loaded from CSV.")
-    except FileNotFoundError:
-        print(f"File '{filename}' not found. No students loaded.")
-def save_students_to_csv(filename):
-    try:
-        with open(filename, 'w', newline='') as file:
-            fieldnames = student_list[0].keys() if student_list else []
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerows(student_list)
-        print("Students saved to CSV.")
-    except Exception as e:
-        print(f"Error saving students to CSV: {e}")
+list = [
+    {"name": "Bob", "phone": "0631234567", "age": 20, "email": "bob@gmail.com"},
+    {"name": "Emma", "phone": "0631234567", "age": 22, "email": "emma@gmail.com"},
+    {"name": "Jon", "phone": "0631234567", "age": 21, "email": "jon@gmail.com"},
+    {"name": "Zak", "phone": "0631234567", "age": 19, "email": "zak@gmail.com"}
+]
 
-def print_all_students():
-    for student in student_list:
-        print(f"Name: {student['name']}, Phone: {student['phone']}, Email: {student['email']}, Group: {student['group']}")
-def add_new_student():
+def save_to_csv(student_list, filename):
+    try:
+        with open(filename, "w", newline="", encoding="utf-8") as csvfile:
+            fieldnames = ["name", "age", "phone", "email"]
+            csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            csvwriter.writeheader()
+            csvwriter.writerows(student_list)
+        print("Data has been saved to the CSV file.")
+    except IOError as e:
+        print(f"Error saving data to {filename}: {e}")
+
+def printAllList():
+    for student in list:
+        strForPrint = f"Student name is {student['name']}, Age is {student['age']}, Phone is {student['phone']}, Email is {student['email']}"
+        print(strForPrint)
+
+def addNewElement():
     name = input("Please enter student name: ")
+    age = int(input("Please enter student age: "))
     phone = input("Please enter student phone: ")
     email = input("Please enter student email: ")
-    group = input("Please enter student group: ")
-    new_student = {"name": name, "phone": phone, "email": email, "group": group}
-    insert_position = 0
-    for student in student_list:
-        if name > student["name"]:
-            insert_position += 1
-        else:
-            break
-    student_list.insert(insert_position, new_student)
-    print("New student has been added")
-def delete_student():
+    new_student = {"name": name, "phone": phone, "age": age, "email": email}
+
+    list.append(new_student)
+    list.sort(key=lambda student: student["name"])
+    print("New element has been added")
+
+def deleteElement():
     name = input("Please enter name to be deleted: ")
-    students_to_delete = [student for student in student_list if student['name'] == name]  
-    if students_to_delete:
-        for student in students_to_delete:
-            student_list.remove(student)
-            print(f"Student '{student['name']}' has been deleted")
+    for student in list:
+        if name == student["name"]:
+            list.remove(student)
+            print("Element has been deleted")
+            break
+    else:
+        print("Element was not found")
+
+def updateElement():
+    name = input("Please enter name to be updated: ")
+    for student in list:
+        if name == student["name"]:
+            student["name"] = input("Enter new name: ")
+            student["age"] = input("Enter new age: ")
+            student["phone"] = input("Enter new phone: ")
+            student["email"] = input("Enter new email: ")
+            list.sort(key=lambda student: student["name"])
+            print("Element has been updated")
+            break
     else:
         print("Student not found")
-def edit_student():
-    name_to_edit = input("Please enter the name of the student to edit:")
-    for student in student_list:
-        if student["name"] == name_to_edit:
-            print(f"Editing student: {student['name']}")
-            new_name = input(f"Enter new name ({student['name']}): ")
-            new_phone = input(f"Enter new phone ({student['phone']}): ")
-            new_email = input(f"Enter new email ({student['email']}): ")
-            new_group = input(f"Enter new group ({student['group']}): ")
-
-            student["name"] = new_name
-            student["phone"] = new_phone
-            student["email"] = new_email
-            student["group"] = new_group
-            print(f"Student '{name_to_edit}' has been updated")
-
-            student_list.sort(key=lambda x: x['name'])
-            print("Student list has been sorted by name.")
-            return
-
-    print(f"Student '{name_to_edit}' not found")
 
 def main():
     while True:
-        choice = input("Please specify the action [C create, D delete, E edit, P print, L load, S save, Q exit]: ")
+        choice = input("Please specify the action [C create, U update, D delete, S save, P print, Q exit]: ")
         if choice.upper() == "C":
-            print("Creating a new student:")
-            add_new_student()
+            print("New element will be created:")
+            addNewElement()
+        elif choice.upper() == "U":
+            print("Existing element will be updated")
+            updateElement()
         elif choice.upper() == "D":
-            print("Deleting a student:")
-            delete_student()
-        elif choice.upper() == "E":
-            print("Editing a student:")
-            edit_student()
+            print("Element will be deleted")
+            deleteElement()
         elif choice.upper() == "P":
-            print("Printing the student list:")
-            print_all_students()
-        elif choice.upper() == "L":
-            filename = input("Please enter the CSV filename to load: ")
-            load_students_from_csv(filename)
+            print("List will be printed")
+            printAllList()
         elif choice.upper() == "S":
-            filename = input("Please enter the CSV filename to save: ")
-            save_students_to_csv(filename)
+            file_name = input("Enter the CSV file name (e.g., lab2.csv) to save data to: ")
+            if not file_name.endswith(".csv"):
+                file_name += ".csv"  
+            save_to_csv(list, file_name) 
         elif choice.upper() == "Q":
             print("Dosviduli")
             break
         else:
-            print("Invalid choice")
+            print("Dosviduli")
 
 if __name__ == "__main__":
     main()
